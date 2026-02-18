d25141b2c648b492a53aadcb0e52e607d68b13a2
Doug Black <doug@dougblack.io>
2026-02-17 21:39:23 -0700

    Update nvim config files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,64 +1,99 @@
--- Plugins --
-require('config.lazy')
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.g.have_nerd_font = true
 
--- Leaders --
--- these leaders are set in ~/.config/nvim/lua/config.lazy.lua
--- vim.g.mapleader = ','
--- vim.g.maplocalleader = ','
-local map = vim.api.nvim_set_keymap
-options = { noremap = true }
-
-map('n', '<leader>ev', ':tab drop ~/.config/nvim/init.lua<cr>', options)
-map('n', '<leader>sv', ':luafile ~/.config/nvim/init.lua<cr>', options)
-map('n', '<leader>ep', ':tab drop ~/.config/nvim/lua/plugins.lua<cr>', options)
-map('n', '<leader>et', ':tab drop ~/.tmux.conf<cr>', options)
-map('n', '<leader>h', ':noh<cr>', options)
-map('n', 'j', 'gj', options)
-map('n', 'k', 'gk', options)
-map('n', '<tab>n', ':tab split<cr>', options)
-map('n', '<tab>q', ':tabc<cr>', options)
-map('n', '<tab>h', ':tabp<cr>', options)
-map('n', '<tab>l', ':tabn<cr>', options)
-map('n', '<S-M-{>', ':tabn<cr>', options)
-map('n', '<leader>\\', ':Neotree<cr>', options)
-map('n', '<leader>-\\', ':Neotree reveal<cr>', options)
-map('n', '<leader>dd', ':lua vim.diagnostic.enable(false, { bufnr = 0 })<cr>', options)
-map('n', '<leader>ed', ':lua vim.diagnostic.enable(true, { bufnr = 0 })<cr>', options)
-map('n', '<leader>b', ':BlameToggle window<cr>', options)
-map('n', '<leader>ai', ':GpChatNew<cr>', options)
-map('n', '<leader>w', ':vsp ~/work.md<cr>', options)
-map('n', '<leader>ca', ':lua vim.lsp.buf.code_action({apply=true})<cr>', options)
-
-map('v', '<leader>y', '"+y', options)
-
-map('t', '<S-Space>', ' ', options)
-map('t', '<S-Enter>', '<cr>', options)
-map('t', '<S-Enter>', '<cr>', options)
-
--- UI --
 vim.opt.number = true
-vim.opt.showcmd = true
-vim.opt.cursorline = true
+
+vim.schedule(function()
+	vim.opt.clipboard = "unnamedplus"
+end)
+
 vim.opt.ignorecase = true
-vim.opt.hidden = true
-vim.opt.wrapscan = true
+vim.opt.smartcase = true
+vim.opt.updatetime = 250
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.opt.inccommand = "split"
 
--- Indentation --
-vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
-vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
-vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
-vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.softtabstop = 2
 
--- Telescope --
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>o', builtin.find_files, options)
-vim.keymap.set('n', '<leader>s', builtin.live_grep, options)
-vim.keymap.set('n', '<leader>fb', builtin.buffers, options)
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, options)
+vim.opt.foldenable = true
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldtext = "v:lua.vim.treesitter.foldtext()"
 
--- Toggleterm
-vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+require("config.lazy")
 
--- Colors --
-vim.o.background = 'dark' -- or 'light' for light mode
-vim.cmd([[colorscheme catppuccin]])
+-- Colors
+vim.cmd.colorscheme("catppuccin")
+
+-- Edit Configs
+vim.keymap.set("n", "<esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set("n", "<leader>ev", ":tabe ~/.config/nvim/init.lua<CR>")
+vim.keymap.set("n", "<leader>sv", ":luafile ~/.config/nvim/init.lua<CR>")
+vim.keymap.set("n", "<leader>ep", ":tabe ~/.config/nvim/lua/plugins/init.lua<CR>")
+vim.keymap.set("n", "<leader>m", ":RenderMarkdown buf_toggle<CR>")
+
+-- Tabs
+vim.keymap.set("n", "<leader><tab>h", ":tabp<CR>")
+vim.keymap.set("n", "<leader><tab>l", ":tabn<CR>")
+
+-- Notes
+vim.keymap.set("n", "<leader>n", ":Neorg workspace notes<CR>")
+
+-- File Explorer
+require("oil").setup()
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+-- Tests
+vim.keymap.set("n", "<leader>tf", ':lua require("neotest").run.run(vim.fn.expand("%"))<CR>')
+vim.keymap.set("n", "<leader>tt", ':lua require("neotest").run.run()<CR>')
+vim.keymap.set("n", "<leader>tr", ':lua require("neotest").summary.toggle()<CR>')
+
+-- Telescope
+local builtin = require("telescope.builtin")
+local function find_by_ext(glob)
+	return function()
+		builtin.find_files({ find_command = { "rg", "--files", "--glob", glob } })
+	end
+end
+
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fp", find_by_ext("*py"), { desc = "Telescope find Python" })
+vim.keymap.set("n", "<leader>fr", find_by_ext("*rs"), { desc = "Telescope find Rust" })
+vim.keymap.set("n", "<leader>ft", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "Telescope help tags" })
+
+-- Diagnostics
+local function diagnostic_toggle(setting)
+	return function()
+		local new_config = not vim.diagnostic.config()[setting]
+		vim.diagnostic.config({ [setting] = new_config })
+	end
+end
+vim.keymap.set("n", "<leader>dd", '<cmd>lua require("telescope.builtin").diagnostics({bufnr=0})<CR>')
+vim.keymap.set("n", "<leader>di", diagnostic_toggle("virtual_text"))
+vim.keymap.set("n", "<leader>dl", diagnostic_toggle("virtual_lines"))
+
+-- Terminal Mode
+vim.keymap.set("t", "<ESC>", "<C-\\><C-n>")
+
+-- LSP configs
+vim.lsp.set_log_level("off")
+
+require("telescope").setup({
+	defaults = {},
+	extensions = {},
+})
+
+vim.api.nvim_create_autocmd("User", {
+	pattern = "TelescopePreviewerLoaded",
+	callback = function(args)
+		vim.wo.number = true
+	end,
+})
